b62f711cf112cc62d99a73b91609c27286ce6a23
Miriam Anyango <minaoanya@gmail.com>
2022-06-08 19:09:29 +0300

    commit tree 2c7719eab1d7651107003564d0930150e90a754d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun(action = "roller-skate"){
+    return (`This Saturday, I want to ${action}!`)
+}
+
+
+function mondayWork(event = "go to the office"){
+    return (`This Monday, I will ${event}.`)
+}
+
+
+/*function demoChain(name) {
+    const part1 = "hi";
+    return function () {
+      const part2 = "there";
+      return function () {
+        console.log(`${part1.toUpperCase()} ${part2} ${name}`);
+      };
+    };
+  }
+  
+  demoChain("Dr. Stephen Strange")()(); //=> HI there Dr. Stephen Strange*/
+
+
+
+  function wrapAdjective(){
+      return function (character = "special") {
+          return `You are ${character}`
+      }
+  }
+  wrapAdjective("%")("a dedicated programmer")
